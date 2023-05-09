b31e67de59caab68d40e9e83dfb05bbc3b5d4cc6
Franco Meinardo <franco.meinardo@silstech.com.ar>
2023-05-09 20:08:49 -0300

    finish

--- src/components/ProgressCircle.jsx
+++ src/components/ProgressCircle.jsx
@@ -5,11 +5,9 @@ const ProgressCircle = ({ progress = "0.75", size = "40" }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const angle = progress * 360;
-
   return (
-    <Box>
-      sx=
-      {{
+    <Box
+      sx={{
         background: `radial-gradient(${colors.primary[400]} 55%, transparent 56%),
                     conic-gradient(transparent 0deg ${angle}deg, ${colors.blueAccent[500]} ${angle}deg 360deg),
                     ${colors.greenAccent[500]}`,
@@ -17,7 +15,7 @@ const ProgressCircle = ({ progress = "0.75", size = "40" }) => {
         width: `${size}px`,
         height: `${size}px`,
       }}
-    </Box>
+    />
   );
 };
 
